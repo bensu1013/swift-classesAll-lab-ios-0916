25bb0c786cf9d675f676e7057ce12f21c54b1a58
Ben Su <bensu1013@hotmail.com>
2016-10-03 10:32:18 -0400

    Jim, I accidentally deleted my old folder and had to redo it.. Sad

--- CatchEmAll/Jet.swift
+++ CatchEmAll/Jet.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+class Jet : Plane {
+    
+    override func climb() {
+        if inFlight {
+            altitude += maxAltitude / 5
+            if altitude > maxAltitude { altitude = maxAltitude }
+            decelerate()
+        }
+    }
+    
+    override func dive() {
+        if altitude > 0 {
+            altitude -= maxAltitude / 5
+            if altitude < 0 { altitude = 0 }
+            accelerate()
+        }
+    }
+    
+    func afterburner() {
+        if inFlight && speed == maxSpeed {
+            speed += speed
+        }
+    }
+    
+}
